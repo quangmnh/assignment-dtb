ebee75320d2863ba42aa29e59c454d4f719b2526
Nhat Thai-Van <79864980+nhat-playmaker@users.noreply.github.com>
2021-11-29 14:03:38 +0700

    Add files via upload

--- mvc/core/db.php
+++ mvc/core/db.php
@@ -13,6 +13,16 @@ class DB {
         mysqli_select_db($this->con, $this->dbname);
         // mysqli_query($this->con, "SET NAMES 'utf8'"); // this only use to display vietnamese
     }
+
+    public function rand_string($length) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
 }
 
 ?>
